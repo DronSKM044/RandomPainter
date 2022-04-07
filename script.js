42e00a2177d905df8655582187f2e814f2f3b547
Andrii Lishchyna <andrej044@i.ua>
2022-04-07 20:52:25 +0200

    add basic code

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 "use strict";
 
-const stage = document.getElementById("canvas-stage");
+const startButton = document.querySelector(".button-start");
 
-const ctx = stage.getContext("2d");
+// Clear our canvas and start painting
+startButton.addEventListener("click", () => {
+  const stage = document.getElementById("canvas-stage");
+  const ctx = stage.getContext("2d");
+  const stageWidth = stage.getBoundingClientRect().width;
+  const stageHeight = stage.getBoundingClientRect().height;
+  ctx.clearRect(0, 0, stageWidth, stageHeight);
+
+  let painter = new Painter(firstColor, secondColor, timer);
+  painter.run();
+});
